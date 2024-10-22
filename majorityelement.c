c7fe3d4d32b0824e2ca49f879167a4e40d3fb745
prakrithi726 <prakrithijain.cs23@bmsce.ac.in>
2024-10-22 22:26:41 +0530

    Create majorityelement.c

---
+++ majorityelement.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int majorityElement(int* nums, int n) {
+    int candidate = nums[0];
+    int count = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (count == 0) {
+            candidate = nums[i];
+            count = 1;
+        } else if (nums[i] == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+}
+
+
+    
+    
