81b9296ce42d8adf73f36c39038584ed109655e1
prakrithi726 <prakrithijain.cs23@bmsce.ac.in>
2024-12-20 23:39:46 +0530

    Update Dfs.c

--- Dfs.c
+++ Dfs.c
@@ -1,57 +1,101 @@
-#include<stdio.h>
-#define MAX 5
-void dfs(int adj[][MAX], int visited[], int start)
-{
-int stack[MAX];
-int top=-1,i,k;
-for(k=0;k<MAX;k++)
 
-visited[k]=0;
-stack[++top]=start;
-visited[start]=1;
+#include <stdio.h>
+#include <stdlib.h>
 
-while(top!=-1)
-{
-start=stack[top--];
-printf("%d->",start);
-for(i=0;i<MAX;i++)
-{
-if( adj[start][i] && visited[i]==0)
-{
-stack[++top]=i;
-visited[i]=1;
-break;
-}
-}
-}
-}
+#define MAX_NODES 100
 
-int main()
-{
-int visited[MAX]={0};
-int adj[MAX][MAX],i,j;
-int option,size;
-do
-{
-printf("\n *********Main Menu************\n");
-printf("\n 1. Enter values in graph ");
-printf("\n 2. DFS Traversal");
+int graph[MAX_NODES][MAX_NODES];
+int visited[MAX_NODES];
+int stack[MAX_NODES];
+int top = -1;
+int nodes;
 
 
-printf("\n \n Enter your choice:");
-scanf("%d",&option);
-switch(option)
-{
-case 1: printf("\n Enter the adjacency matrix:");
-for(i=0;i<MAX;i++)
-for(j=0;j<MAX;j++)
-scanf("%d",&adj[i][j]);
-break;
 
-case 2: printf("DFS Traversal:");
-dfs(adj,visited,0);
-break;
+
+
+int DFS(int startNode) {
+    int visitedCount = 0;
+    stack[++top] = startNode;
+
+    while (top != -1) {
+        int node = stack[top--];
+
+        if (!visited[node]) {
+            visited[node] = 1;
+            visitedCount++;
+            for (int i = 0; i < nodes; i++) {
+                if (graph[node][i] == 1 && !visited[i]) {
+                    stack[++top] = i;
+                }
+            }
+        }
+    }
+    return visitedCount;
 }
-}while(option!=4);
-return 0;
+
+
+
+
+
+
+int main() {
+    int edges;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &nodes);
+
+    printf("Enter the number of edges: ");
+    scanf("%d", &edges);
+
+    for (int i = 0; i < nodes; i++) {
+        for (int j = 0; j < nodes; j++) {
+            graph[i][j] = 0;
+        }
+    }
+
+    printf("Enter the edges (u v) where u and v are node indices starting from 0:\n");
+    for (int i = 0; i < edges; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        graph[u][v] = 1;
+        graph[v][u] = 1;
+    }
+
+    for (int i = 0; i < nodes; i++) {
+        visited[i] = 0;
+    }
+
+    int visitedNodes = DFS(0);
+
+    if (visitedNodes == nodes) {
+        printf("The graph is connected.\n");
+    } else {
+        printf("The graph is not connected.\n");
+    }
+
+    return 0;
 }
+
+
+
+
+
+
+output - --------
+Enter the number of nodes: 4
+Enter the number of edges: 3
+Enter the edges (u v) where u and v are node indices starting from 0:
+0 1
+1 2
+2 3
+The graph is connected.
+
+Enter the number of nodes: 5
+Enter the number of edges: 3
+Enter the edges (u v) where u and v are node indices starting from 0:
+0 1
+1 2
+3 4
+The graph is not connected.
+
